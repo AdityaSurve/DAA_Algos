8929a3159d426d8d4c895bfb9cff74a4a35c853f
AdityaSurve <surveaditya521@gmail.com>
2023-05-17 22:02:55 +0530

    Greedy9

--- MST(Prims).C
+++ MST(Prims).C
@@ -1,68 +1,78 @@
-// prims algorithm
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <limits.h>
 
-#define MAX_CITIES 10
+#define MAX_VERTICES 100
 
-int numCities;
-int graph[MAX_CITIES][MAX_CITIES];
-bool visited[MAX_CITIES];
-int bestPath[MAX_CITIES + 1];
-int minCost = INT_MAX;
+int numVertices;
+int graph[MAX_VERTICES][MAX_VERTICES];
 
 void inputGraph() {
-    printf("Enter the number of cities: ");
-    scanf("%d", &numCities);
+    printf("Enter the number of vertices: ");
+    scanf("%d", &numVertices);
 
-    printf("Enter the cost matrix:\n");
-    for (int i = 0; i < numCities; i++) {
-        for (int j = 0; j < numCities; j++) {
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
             scanf("%d", &graph[i][j]);
         }
     }
 }
 
-void printSolution() {
-    printf("Optimal Path: ");
-    for (int i = 0; i <= numCities; i++) {
-        printf("%d ", bestPath[i]);
-    }
-    printf("\n");
+int findMinKey(int key[], bool mstSet[]) {
+    int minKey = INT_MAX;
+    int minIndex = -1;
 
-    printf("Minimum Cost: %d\n", minCost);
+    for (int v = 0; v < numVertices; v++) {
+        if (mstSet[v] == false && key[v] < minKey) {
+            minKey = key[v];
+            minIndex = v;
+        }
+    }
+
+    return minIndex;
 }
 
-void branchAndBoundTSP(int currCity, int level, int currCost) {
-    if (level == numCities) {
-        if (graph[currCity][0] != 0 && currCost + graph[currCity][0] < minCost) {
-            minCost = currCost + graph[currCity][0];
-            bestPath[level] = 0;
-            bestPath[numCities] = minCost;
-        }
-        return;
+void printMST(int parent[]) {
+    printf("Edge   Weight\n");
+    for (int v = 1; v < numVertices; v++) {
+        printf("%d - %d    %d\n", parent[v], v, graph[v][parent[v]]);
+    }
+}
+
+void primMST() {
+    int parent[MAX_VERTICES];
+    int key[MAX_VERTICES];
+    bool mstSet[MAX_VERTICES];
+
+    for (int i = 0; i < numVertices; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
     }
 
-    for (int i = 0; i < numCities; i++) {
-        if (!visited[i] && graph[currCity][i] != 0) {
-            visited[i] = true;
-            bestPath[level] = i;
-            branchAndBoundTSP(i, level + 1, currCost + graph[currCity][i]);
-            visited[i] = false;
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < numVertices - 1; count++) {
+        int u = findMinKey(key, mstSet);
+
+        mstSet[u] = true;
+
+        for (int v = 0; v < numVertices; v++) {
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
         }
     }
+
+    printMST(parent);
 }
 
 int main() {
     inputGraph();
 
-    visited[0] = true;
-    bestPath[0] = 0;
-
-    branchAndBoundTSP(0, 1, 0);
-
-    printSolution();
+    primMST();
 
     return 0;
 }
